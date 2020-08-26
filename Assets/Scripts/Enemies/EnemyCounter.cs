ce9fefb304e79531c1893393cd00f112b0b0b513
jkinghgp17 <jck4294@gmail.com>
2020-08-26 16:35:55 -0400

    Added more enemies and Started Enemy Counter

---
+++ Assets/Scripts/Enemies/EnemyCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCounter : MonoBehaviour
+{
+    public Text ec;
+
+    public int maxEnemies = 4;
+    public int curEnemies;
+
+    void Start()
+    {
+        curEnemies = maxEnemies;
+    }
+
+    void Update()
+    {
+        ec.text = "Enemies: " + curEnemies + "/" + maxEnemies;
+    }
+
+    public void KillEnemy()
+    {
+        curEnemies--;
+    }
+}
